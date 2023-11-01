9d8ba3bea8c488738fd84c96b48817438c36b4c8
parshpatel <parshpat@gmail.com>
2023-10-31 21:42:04 -0400

    my updated code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #this is my forst githhub addotion
+
+
 def decode_password(password):
     decode_password = ""
     for digit in password:
@@ -11,4 +13,58 @@ def decode_password(password):
             decode_password -= digit
 
     return decode_password
+
+
+
+def encoder(password):
+    encoded_string = ''
+    password_list = list(password)
+    for i in range(0, len(password_list)):
+        if password_list[i] == '1':
+            encoded_string += '4'
+        if password_list[i] == '2':
+            encoded_string += '5'
+        if password_list[i] == '3':
+            encoded_string += '6'
+        if password_list[i] == '4':
+            encoded_string += '7'
+        if password_list[i] == '5':
+            encoded_string += '8'
+        if password_list[i] == '6':
+            encoded_string += '9'
+        if password_list[i] == '7':
+            encoded_string += '0'
+        if password_list[i] == '8':
+            encoded_string += '1'
+        if password_list[i] == '9':
+            encoded_string += '2'
+
+    return encoded_string
+
+def main():
+    while True:
+        print('Menu')
+        print('-' * 13)
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        user_select = int(input('Please enter an option:'))
+        password_select = (input('Please enter password to encode:'))
+        if user_select == 1:
+            y = encoder(password_select)
+            print(y)
+            print('Your password has been stored and encoded!')
+        elif user_select == 2:
+            decoded_password = decode_password(password_select)
+            print(decoded_password)
+        elif user_select == 3:
+            break
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
 
